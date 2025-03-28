ea00c97f62280dc95b8da079641ed8ef7b085746
KhoaEban <khoaebanypk03641@gmail.com>
2025-03-28 17:58:17 +0700

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,9 @@ use App\Http\Controllers\User\SupportController;
 use App\Http\Controllers\User\SimulationController;
 
 // Instructor
-use App\Http\Controllers\Instructor\HomeControllerInstructor;
-use App\Http\Controllers\Instructor\CourseControllerTeacher;
-use App\Http\Controllers\Instructor\LessonController;
-
-// Trang chủ
-Route::get('/', [HomeController::class, 'index'])->name('home');
+use App\Http\Controllers\Teacher\HomeControllerInstructor;
+use App\Http\Controllers\Teacher\CourseControllerTeacher;
+use App\Http\Controllers\Teacher\LessonController;
 // Trang chủ
 Route::get('/', [HomeController::class, 'index']);
 
@@ -69,21 +66,6 @@ Route::middleware(['check.role:admin'])->group(function () {
         Route::post('/assignChild', [CategoryControllerAdmin::class, 'assignChild'])->name('admin.category.assignChild');
         Route::delete('/unlink/{id}', [CategoryControllerAdmin::class, 'unlinkCategory'])->name('admin.category.unlink');
     });
-
-});
-
-
-// Instructor
-Route::middleware(['check.role:instructor'])->group(function () {
-    Route::get('/instructor/home', [HomeControllerInstructor::class, 'index'])->name('instructor.dashboard');
-
-});
-
-
-// User
-Route::prefix('user')->group(function () {
-    Route::get('/home', [HomeController::class, 'index'])->name('home');
-
 });
 
 
@@ -121,7 +103,6 @@ Route::prefix('user')->group(function () {
     Route::get('/category/{slug}', [CategoryController::class, 'show'])->name('category.show');
 
     Route::get('/course', [CourseController::class, 'index'])->name('course');
-
     Route::get('/course/{slug}', [CourseController::class, 'show'])->name('course.show');
     Route::get('/lesson', [LessonController::class, 'index'])->name('lessons');
     Route::get('/lesson/{id}', [LessonController::class, 'show'])->name('lessons.show');
