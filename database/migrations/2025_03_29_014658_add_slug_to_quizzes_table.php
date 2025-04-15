b0e75c59ee169a9987177e86b88c26d35c5bc59a
Z1ted <160805456+Z1ted@users.noreply.github.com>
2025-04-15 09:56:58 +0700

    sủa lại migrate

--- database/migrations/2025_03_29_014658_add_slug_to_quizzes_table.php
+++ database/migrations/2025_03_29_014658_add_slug_to_quizzes_table.php
@@ -8,7 +8,9 @@ return new class extends Migration {
     public function up()
     {
         Schema::table('quizzes', function (Blueprint $table) {
-            $table->string('slug')->nullable()->after('title');
+            if (!Schema::hasColumn('quizzes', 'slug')) {
+                $table->string('slug')->nullable()->after('title');
+            }
         });
     }
 
